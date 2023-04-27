ecb91da8727ea64b7ee58fa417b1fe643c719094
klacabane <kevin.lacabane@elastic.co>
2023-04-27 23:39:02 +0200

    some types

--- types.ts
+++ types.ts
@@ -3,11 +3,17 @@ export interface SimpleAsset<T> {
   'asset.ean': string;
   'asset.type': T;
   'asset.id': string;
+  'asset.kind': string;
   'asset.name'?: string;
   'asset.parents'?: string[];
   'asset.children'?: string[];
   'asset.references'?: string[];
   'cloud.provider'?: string;
+  'cloud.service.name'?: string;
+  'cloud.region'?: string;
+  'cloud.instance.id'?: string;
   'orchestrator.cluster.name'?: string;
   'service.environment'?: string;
 }
+
+export type HostType = 'host' | 'aws.ec2' | 'k8s.node' | 'gcp.gce';
